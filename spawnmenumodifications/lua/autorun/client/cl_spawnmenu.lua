c46dfd1da9406b458725ea087b0a9ffec90dfe21
Michael <88868859+SameDragon1@users.noreply.github.com>
2021-11-21 16:21:33 -0500

    Update cl_spawnmenu.lua

--- spawnmenumodifications/lua/autorun/client/cl_spawnmenu.lua
+++ spawnmenumodifications/lua/autorun/client/cl_spawnmenu.lua
@@ -7,7 +7,6 @@ if table.HasValue( SAMEDRAGON_SPAWNMENU_CONFIG_ADMINS, LocalPlayer() :GetUserGro
             v.Tab:GetText() == language.GetPhrase("spawnmenu.category.weapons") or
             v.Tab:GetText() == language.GetPhrase("spawnmenu.category.vehicles") or
             v.Tab:GetText() == language.GetPhrase("spawnmenu.category.postprocess") or
-            v.Tab:GetText() == language.GetPhrase("spawnmenu.category.props") or
             v.Tab:GetText() == language.GetPhrase("spawnmenu.category.dupes") or
             v.Tab:GetText() == language.GetPhrase("spawnmenu.category.saves")) then
             g_SpawnMenu.CreateMenu:CloseTab( v.Tab, true )
